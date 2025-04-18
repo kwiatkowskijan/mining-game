d0c5fce4f54875f2b1b7fae80cbbc6162adad4a7
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-04-18 13:30:33 +0200

    Set up scene

---
+++ Assets/Player/Movement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace MiningGame.Assets.Player
+{
+    public class Movement : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
