17cde4cb47e423fe03dcf648fbd6e3d1e8e4ad25
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-09-28 17:45:06 +0200

    feat(WorldGeneration): generate world by chunks #19

--- Assets/Scripts/WorldGeneration/MineGenerator.cs
+++ Assets/Scripts/WorldGeneration/MineGenerator.cs
@@ -7,32 +7,56 @@ namespace MiningGame.WorldGeneration
 {
     public class MineGenerator : MonoBehaviour
     {
-        [SerializeField] private Tilemap caveTilemap;
+        private Tilemap _caveTilemap;
         [SerializeField] private List<OreTile> oreTiles;
         [SerializeField] private List<DirtTile> dirtTiles;
         [SerializeField] private int mapHeight;
         [SerializeField] private int mapWidth;
         [SerializeField] private Vector3Int startPosition = new Vector3Int(0, 0, 0);
         [SerializeField] private float perlinNoiseScale;
+        [SerializeField] private int chunkSize = 16;
+
 
         private void Awake()
+        {
+            _caveTilemap = GetComponentInChildren<Tilemap>();
+        }
+
+        private void Start()
         {
             GenerateCave(mapWidth, mapHeight);
         }
 
         private void GenerateCave(int width, int height)
         {
-            for (int x = 0; x < width; x++)
+            int chunksX = Mathf.CeilToInt((float)width / chunkSize);
+            int chunksY = Mathf.CeilToInt((float)height / chunkSize);
+
+            for (int x = 0; x < chunksX; x++)
             {
-                for (int y = 0; y < height; y++)
+                for (int y = 0; y < chunksY; y++)
+                {
+                    GenerateChunk(x, y);
+                }
+            }
+        }
+
+        private void GenerateChunk(int chunkX, int chunkY)
+        {
+            int startX = chunkX * chunkSize;
+            int startY = chunkY * chunkSize;
+
+            for (int x = startX; x < startX + chunkSize; x++)
+            {
+                for (int y = startY; y < startY + chunkSize; y++)
                 {
                     Vector3Int tilePosition = new Vector3Int(startPosition.x + x, startPosition.y - y, 0);
                     float noise = Mathf.PerlinNoise(x * perlinNoiseScale, y * perlinNoiseScale);
-                    
+
                     if (noise > 0.7f)
-                        caveTilemap.SetTile(tilePosition, oreTiles.Find(t => t.isDescrutable).tile);
+                        _caveTilemap.SetTile(tilePosition, oreTiles.Find(t => t.isDescrutable).tile);
                     else
-                        caveTilemap.SetTile(tilePosition, dirtTiles.Find(t => t.isDescrutable).tile);
+                        _caveTilemap.SetTile(tilePosition, dirtTiles.Find(t => t.isDescrutable).tile);
                 }
             }
         }
