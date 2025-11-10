fe4b13876535de9e0574218de8c98885e671f5b3
malpish0n <akasztelan617@gmail.com>
2025-11-10 11:21:34 +0100

    feat(shop): ShopTrigger, E tooltip, Shop open

---
+++ Assets/Scripts/Shop/ShopItemType.cs
@@ -0,0 +1,12 @@
+﻿namespace MiningGame
+{
+    public enum ShopItemType
+    {
+        Tool,
+        Weapon,
+        Armor,
+        Mineral,
+        Misc     // rails, ladders etc.
+    }
+}
+
---
+++ Assets/Scripts/Shop/ShopTrigger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+
+namespace MiningGame
+{
+    public class ShopTrigger : MonoBehaviour
+    {
+        [Header("UI References")]
+        [SerializeField] private GameObject shopUI;
+        [SerializeField] private GameObject tooltipE;
+        
+        private bool playerInRange = false;
+        private bool shopOpen = false;
+        
+        private void Start()
+        {
+            if (shopUI != null)
+                shopUI.SetActive(false);
+                
+            if (tooltipE != null)
+                tooltipE.SetActive(false);
+        }
+        
+        private void Update()
+        {
+            if (playerInRange && Input.GetKeyDown(KeyCode.E) && !shopOpen)
+            {
+                OpenShop();
+                Debug.Log("Shop opened");
+            }
+            
+            if (shopOpen && Input.GetKeyDown(KeyCode.Escape))
+            {
+                CloseShop();
+                Debug.Log("Shop closed");
+            }
+        }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                playerInRange = true;
+                if (tooltipE != null && !shopOpen)
+                    tooltipE.SetActive(true);
+            }
+        }
+        
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                playerInRange = false;
+                if (tooltipE != null)
+                    tooltipE.SetActive(false);
+            }
+        }
+        
+        private void OpenShop()
+        {
+            shopOpen = true;
+            if (shopUI != null)
+                shopUI.SetActive(true);
+                
+            if (tooltipE != null)
+                tooltipE.SetActive(false);
+        }
+        
+        private void CloseShop()
+        {
+            shopOpen = false;
+            if (shopUI != null)
+                shopUI.SetActive(false);
+                
+            if (tooltipE != null && playerInRange)
+                tooltipE.SetActive(true);
+        }
+    }
+}
+
+
