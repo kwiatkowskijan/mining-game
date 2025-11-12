306d89eecd41fda2ff747b1ddcd409c8540384a8
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-11-12 16:45:08 +0100

    refactor: project structure

--- Assets/Scripts/Core/Bootstrap.cs
+++ Assets/Scripts/Core/Bootstrap.cs
@@ -19,9 +19,12 @@ namespace MiningGame.Core
         private void Awake()
         {
             DontDestroyOnLoad(gameObject);
+
             ServiceLocator.Register<IMineralsService>(new MineralsService(mineralsDatabase));
             ServiceLocator.Register<IAudioService>(new AudioService(sfxSource, musicSource));
             ServiceLocator.Register<ISceneService>(new SceneService());
+
+            Debug.Log("[Bootstrap] All services registered!");
         }
     }
 }
