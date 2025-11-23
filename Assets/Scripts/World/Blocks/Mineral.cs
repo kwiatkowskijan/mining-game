a32c89e6c7e76739a44808d9ffc6791147b5ff51
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2025-11-23 18:42:31 +0100

    feat(Minerals): added all Minerals SO's #70

--- Assets/Scripts/World/Blocks/Mineral.cs
+++ Assets/Scripts/World/Blocks/Mineral.cs
@@ -2,18 +2,26 @@ using UnityEngine;
 
 namespace MiningGame.WorldGeneration
 {
-    [CreateAssetMenu(fileName = "Ore", menuName = "Scriptable Objects/Block/Ore")]
+    [CreateAssetMenu(fileName = "Mineral", menuName = "Scriptable Objects/Block/Mineral")]
     public class Mineral : Block
     {
         public string mineralName;
         [Range(1, 100)]
-        public int commonness;
-        public int minDepth;
-        public int maxDepth;
-        public int rarity;
-        public float miningMultiplier;
+        public int commonness = 50;
+        public int minDepth = 100;
+        public int maxDepth = -100;
+        // public int rarity;
+        public float miningMultiplier = 1.0f;
         public GameObject toSpawn;
         public Sprite icon;
         public bool isDiscovered = false;
+
+        private void OnValidate()
+        {
+            if (string.IsNullOrEmpty(mineralName) || mineralName != this.name)
+            {
+                mineralName = this.name;
+            }
+        }
     }
 }
